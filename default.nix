5e3fbb8504508d19e7970463d743c38228f32363
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2017-03-28 14:08:11 +0300

    build:  update to a more modern version of "right"

--- default.nix
+++ default.nix
@@ -1,57 +1,30 @@
-# Install with:
-#  nix-env --no-build-output --cores 4 -iE 'f: (import ./. {})'
-
-with import <nixpkgs> {};
-let
-  default_compiler = "ghc7103";
-  youtrack-src-github =
-    pkgs.fetchgit {
-      url    = https://github.com/deepfire/youtrack;
-      rev    = "39eebdc7540183da75f97021a18418e9b67fbb77";
-      sha256 = "0lc22d68zgpk38q67gm013pzi3d9n2imp7y2kfvsjcslxigygci9";
-    };
-  youtrack-src-local = ../youtrack;
-  youtrack = hpkgs: src:
-             with hpkgs;
-             hpkgs.mkDerivation {
-               pname = "youtrack";
-               version = "0.0.6";
-               src = src;
-               libraryHaskellDepends = [
-                 aeson base base-unicode-symbols bytestring HsOpenSSL http-client
-                 http-client-openssl lens mtl parsers QuickCheck safe scientific split text time trifecta
-                 unordered-containers utf8-string vector wreq
-               ];
-               homepage = "https://github.com/deepfire/youtrack";
-               description = "Access a Jetbrains YouTrack instance";
-               license = stdenv.lib.licenses.gpl3;
-             };
-in
-
-{ haskellPackages ? pkgs.haskell.packages.${default_compiler}
-, youtrack-from-github ? true }:
-let
-  youtrack-src    = if youtrack-from-github
-		    then youtrack-src-github
-		    else youtrack-src-local;
-  hpkgs           = haskellPackages.override (oldArgs: {
-    overrides = with haskell.lib; new: old:
-    let parent = (oldArgs.overrides or (_: _: {})) new old;
-    in parent // {
- #     brick = old.brick_0_5;
-    };
-  });
-in
-with hpkgs;
+{ mkDerivation, stdenv, src ? ./.
+, base, base-unicode-symbols, brick, bytestring
+, cabal-install
+, cassava, configurator-export, containers
+, data-default
+, exceptions
+, free-vl
+, ghc-core, gitlib, gitlib-libgit2
+, http-client, http-client-openssl
+, linear
+, newtype
+, parsers, pkgconfig, profunctors
+, QuickCheck
+, singletons, servant-client, split
+, time, trifecta, turtle
+, vector
+, wreq
+##########
+, youtrack
+}:
 mkDerivation {
-  pname        = "youtrack-tools";
+  pname        = "corr";
   version      = "0.0.7";
   src          = ./.;
   isLibrary    = false;
   isExecutable = true;
-  # haskell-src-meta -> configuration-tools, configifier
-  buildDepends = # (if haskellPackages.ghc != pkgs.haskell.packages.ghc801.ghc then [ cabal-install ] else []) ++
-                 [ base base-unicode-symbols brick bytestring
+  buildDepends = [ base base-unicode-symbols brick bytestring
 		   cabal-install
 		   cassava configurator-export containers
 		   data-default
@@ -68,8 +41,8 @@ mkDerivation {
 		   vector
 		   wreq
 		   ###
-		   (youtrack hpkgs youtrack-src)
+		   youtrack
 		 ];
-  description  = "Correlate JetBrains Youtrack issues and git branches.";
+  description  = "Correlate JetBrains Youtrack project and a git repository.";
   license      = stdenv.lib.licenses.gpl3;
 }
--- shell.nix
+++ shell.nix
@@ -1,24 +1,51 @@
-{ nixpkgs ? import <nixpkgs> {}
-, compiler ? "ghc801"
-, youtrack-from-github ? false }:
+# Install with:
+#  nix-env --no-build-output --cores 4 -iE 'f: (import ./. {})'
 
+{ nixpkgs     ? import <nixpkgs> {}
+, pkgs        ? nixpkgs.pkgs, haskell ? pkgs.haskell
+, compiler    ? "ghc802"
+, ghcOrig     ? pkgs.haskell.packages."${compiler}"
+, localYT     ? false
+}:
 let
+  overcabal = pkgs.haskell.lib.overrideCabal;
+  hubsrc    =      repo: rev: sha256:       pkgs.fetchgit { url = "https://github.com/" + repo; rev = rev; sha256 = sha256; };
+  overc     = old:                    args: overcabal old (oldAttrs: (oldAttrs // args));
+  overhub   = old: repo: rev: sha256: args: overc old ({ src = hubsrc repo rev sha256; }       // args);
+  overhage  = old: version:   sha256: args: overc old ({ version = version; sha256 = sha256; } // args);
 
-  inherit (nixpkgs) pkgs;
-
-  f = import ./.; # your default.nix
-
-  haskell             = pkgs.haskell;
-  haskellPackagesOrig = haskell.packages.${compiler};
-
-  haskellPackages     = # import ../nixpkgs-haskellpackages-overrides/ghc801.nix pkgs
-                        haskellPackagesOrig;
-
-  drv = haskellPackages.callPackage f {
-    haskellPackages = haskellPackages;
-    youtrack-from-github = youtrack-from-github;
+  youtrack-src-github = pkgs.fetchgit {
+    url    = https://github.com/deepfire/youtrack;
+    rev    = "39eebdc7540183da75f97021a18418e9b67fbb77";
+    sha256 = "0lc22d68zgpk38q67gm013pzi3d9n2imp7y2kfvsjcslxigygci9";
   };
+  youtrack-src-local  = ../youtrack;
 
-in
+  ghc       = ghcOrig.override (oldArgs: {
+    overrides = with haskell.lib; new: old:
+    let parent = (oldArgs.overrides or (_: _: {})) new old;
+    in with new; parent // {
+      youtrack = new.mkDerivation {
+        pname = "youtrack";
+        version = "0.0.6";
+        src = if !localYT then youtrack-src-github else youtrack-src-local;
+        libraryHaskellDepends = [
+          aeson base base-unicode-symbols bytestring HsOpenSSL http-client
+          http-client-openssl lens mtl parsers QuickCheck safe scientific split text time trifecta
+          unordered-containers utf8-string vector wreq
+        ];
+        homepage = "https://github.com/deepfire/youtrack";
+        description = "Access a Jetbrains YouTrack instance";
+        license = stdenv.lib.licenses.gpl3;
+      };
+    };
+  });
 
-  if pkgs.lib.inNixShell then drv.env else drv
+  ###
+  nakeDrv = ghc.callPackage (import ./.) {};
+  drv = (haskell.lib.addBuildTools nakeDrv
+         [ pkgs.cabal-install
+           pkgs.stack
+           ghc.intero
+         ]);
+in if pkgs.lib.inNixShell then drv.env else drv
