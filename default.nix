7d09c2cb199229608eba3f1b95eba52a881867a1
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-08-02 00:17:16 +0300

    Generate default.nix from corr.cabal

--- default.nix
+++ default.nix
@@ -1,48 +1,23 @@
-{ mkDerivation, stdenv, src ? ./.
-, base, base-unicode-symbols, brick, bytestring
-, cabal-install
-, cassava, configurator-export, containers
-, data-default
-, exceptions
-, free-vl
-, ghc-core, gitlib, gitlib-libgit2
-, http-client, http-client-openssl
-, linear
-, newtype
-, parsers, pkgconfig, profunctors
-, QuickCheck
-, singletons, servant-client, split
-, time, trifecta, turtle
-, vector
-, wreq
-##########
-, youtrack
+{ mkDerivation, aeson, base, base-unicode-symbols, brick
+, bytestring, cassava, containers, data-default, exceptions, foldl
+, free-vl, gitlib, gitlib-libgit2, hashable, lens, monad-control
+, mtl, optparse-applicative, QuickCheck, scientific, split, stdenv
+, tagged, text, time, turtle, unordered-containers, utf8-string
+, vector, vty, wreq, youtrack
 }:
 mkDerivation {
-  pname        = "corr";
-  version      = "0.0.7";
-  src          = ./.;
-  isLibrary    = false;
+  pname = "corr";
+  version = "0.0.7";
+  src = ./.;
+  isLibrary = false;
   isExecutable = true;
-  buildDepends = [ base base-unicode-symbols brick bytestring
-		   cabal-install
-		   cassava configurator-export containers
-		   data-default
-		   exceptions
-		   free-vl
-		   ghc-core gitlib gitlib-libgit2
-		   http-client http-client-openssl
-		   linear
-		   newtype
-		   parsers pkgconfig profunctors
-		   QuickCheck
-		   singletons servant-client split
-		   time trifecta turtle
-		   vector
-		   wreq
-		   ###
-		   youtrack
-		 ];
-  description  = "Correlate JetBrains Youtrack project and a git repository.";
-  license      = stdenv.lib.licenses.gpl3;
+  executableHaskellDepends = [
+    aeson base base-unicode-symbols brick bytestring cassava containers
+    data-default exceptions foldl free-vl gitlib gitlib-libgit2
+    hashable lens monad-control mtl optparse-applicative QuickCheck
+    scientific split tagged text time turtle unordered-containers
+    utf8-string vector vty wreq youtrack
+  ];
+  description = "Correlate JetBrains Youtrack issues and git branches";
+  license = stdenv.lib.licenses.gpl3;
 }
